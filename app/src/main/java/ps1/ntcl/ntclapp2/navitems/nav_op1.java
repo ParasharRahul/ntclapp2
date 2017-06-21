a9497b5a3a4b59a4592716493e2d79b4dd49ca6e
Aditya Raj Agarwal <aragarwal1997@gmail.com>
2017-06-21 16:20:45 +0530

    blank java files for tabs inserted

---
+++ app/src/main/java/ps1/ntcl/ntclapp2/navitems/nav_op1.java
@@ -0,0 +1,22 @@
+package ps1.ntcl.ntclapp2.navitems;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ps1.ntcl.ntclapp2.R;
+
+public class nav_op1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_nav_op1, container, false);
+
+        return rootView;
+    }
+
+
+}
---
+++ app/src/main/java/ps1/ntcl/ntclapp2/navitems/nav_subop2.java
@@ -0,0 +1,24 @@
+package ps1.ntcl.ntclapp2.navitems;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ps1.ntcl.ntclapp2.R;
+
+public class nav_subop2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_nav_subop2, container, false);
+
+        return rootView;
+    }
+
+
+}
